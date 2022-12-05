b0e3876afb4c2bbc7a2114c8a59ed7aa48ce24be
Nurtal <nathan.foulquier.pro@gmail.com>
2022-12-05 11:26:26 +0100

    synchro

--- matching_groups.py
+++ matching_groups.py
@@ -1,4 +1,7 @@
-
+## importation
+import os
+import shutil
+import pandas as pd
 
 
 
@@ -140,6 +143,114 @@ def run_matching(data_file, work_folder):
 
 
 
+def home_made_appariement(data_file, work_folder):
+    """
+    Because in the end, intuituve is not always stupid
+    """
+
+    ## parameters
+    id_save_list = work_folder+"/pairing_log/matched_id_list.csv"
+    distribution_save_file = work_folder+"/pairing_log/distribution_check.png"
+    pair_list = []
+    pos_neg_to_dist = {}
+    pos_to_neg_to_dist = {}
+    age_penality = 1
+    sex_penality = 5
+
+    ## clean & prepare output folde
+    if(not os.path.isdir(work_folder+"/pairing_log")):
+        os.mkdir(work_folder+"/pairing_log")
+    else:
+        shutil.rmtree(work_folder+"/pairing_log")
+        os.mkdir(work_folder+"/pairing_log")
+
+    ## load dataset
+    df = pd.read_csv(data_file)
+
+    ## consider only age and gender
+    df = df[["ID","SEX","AGE","LABEL"]]
+
+    ## split into pos and neg
+    df_pos = df[df["LABEL"] == 1]
+    df_neg = df[df["LABEL"] == 0]
+
+    ## loop over pos patient
+    for index, row in df_pos.iterrows():
+
+        id = row["ID"]
+        a1 = row["AGE"]
+        s1 = row["SEX"]
+
+        #-> loop over neg
+        for i2, r2 in df_neg.iterrows():
+
+            id2 = r2["ID"]
+            a2 = r2["AGE"]
+            s2 = r2["SEX"]
+
+            #-> compute distance
+            dist = (abs(int(a1)-int(a2)))*age_penality + (abs(int(s1) - int(s2)))*sex_penality
+
+            #-> update matrix distance
+            key = str(id)+"_"+str(id2)
+            inverse_key = str(id2)+"_"+str(id)
+            if(key not in pos_neg_to_dist.keys() and inverse_key not in pos_neg_to_dist.keys()):
+                pos_neg_to_dist[key] = dist
+
+            #-> update matrix distance 2
+            if(id in pos_to_neg_to_dist.keys()):
+                pos_to_neg_to_dist[id][id2] = dist
+            else:
+                pos_to_neg_to_dist[id] = {}
+                pos_to_neg_to_dist[id][id2] = dist
+
+
+    ## loop over the matrix, first id of the key is supposed to be the pos id (i.e for us the patient with disease, assume that those guys are more rare then control)
+    ## sort dictionnary
+    """
+    pos_neg_to_dist = {k: v for k, v in sorted(pos_neg_to_dist.items(), key=lambda item: item[1])}
+    matched_patient = []
+    for k in pos_neg_to_dist.keys():
+        k_array = k.split("_")
+        pos = k_array[0]
+        neg = k_array[1]
+        pos = pos.replace(".0", "")
+        neg = neg.replace(".0", "")
+        dist = pos_neg_to_dist[k]
+        if(pos not in matched_patient and neg not in matched_patient):
+            matched_patient.append(pos)
+            matched_patient.append(neg)
+    """
+
+    neg_patient_used = []
+    for pos in pos_to_neg_to_dist.keys():
+        neg_to_dist = pos_to_neg_to_dist[pos]
+        neg_to_dist = {k: v for k, v in sorted(neg_to_dist.items(), key=lambda item: item[1])}
+        neg_candidates = list(neg_to_dist.keys())
+        for neg in neg_candidates:
+            if(neg not in neg_patient_used):
+                neg_patient_used.append(neg)
+                print("-> "+str(pos)+" - "+str(neg))
+                break
+
+
+    ## craft id final list
+    final_list = neg_patient_used
+    for id in list(pos_to_neg_to_dist.keys()):
+        final_list.append(id)
+
+    ## craft final dataframe
+    df = pd.read_csv(data_file)
+    df = df[df["ID"].isin(final_list)]
+    print(df)
+
+    ## write csv
+    df.to_csv("data_picvert_processed_apparied.csv", index=False)
+
+
+
 
 #data_file = "/home/bran/Workspace/PRECISINV/fatigue/labels/matching_patients.csv"
-#run_matching(data_file, "/home/bran/Workspace/misc/murloc_test6")
+data_file = "/home/bran/Workspace/SIDEQUEST/Bene/picvert_in_R/data_picvert_processed_for_appariement.csv"
+#run_matching(data_file, "/home/bran/Workspace/SIDEQUEST/Bene/picvert_in_R/appariement")
+home_made_appariement(data_file, "/home/bran/Workspace/SIDEQUEST/Bene/picvert_in_R/appariement")
