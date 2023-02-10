85bad8a73fd42523b2a79c4d79fd70adb6a7643b
Nurtal <nathan.foulquier.pro@gmail.com>
2023-02-10 15:55:54 +0100

    up

--- clf/clf_rf.py
+++ clf/clf_rf.py
@@ -1,8 +1,5 @@
-
-
 def run_rf_classifier(input_file, work_folder):
-    """
-    """
+    """ """
 
     ## importation
     import os
@@ -20,16 +17,16 @@ def run_rf_classifier(input_file, work_folder):
     import numpy as np
 
     ## parameters
-    rf_save_name = work_folder+"/rf_log/rf_model.joblib"
-    rf_confusion_save_file = work_folder+"/rf_log/rf_confusion_matrix.png"
-    log_file_name = work_folder+"/rf_log/rf_evaluation.log"
+    rf_save_name = work_folder + "/rf_log/rf_model.joblib"
+    rf_confusion_save_file = work_folder + "/rf_log/rf_confusion_matrix.png"
+    log_file_name = work_folder + "/rf_log/rf_evaluation.log"
 
     ## clean & prepare output folde
-    if(not os.path.isdir(work_folder+"/rf_log")):
-        os.mkdir(work_folder+"/rf_log")
+    if not os.path.isdir(work_folder + "/rf_log"):
+        os.mkdir(work_folder + "/rf_log")
     else:
-        shutil.rmtree(work_folder+"/rf_log")
-        os.mkdir(work_folder+"/rf_log")
+        shutil.rmtree(work_folder + "/rf_log")
+        os.mkdir(work_folder + "/rf_log")
 
     ## load file
     df = pd.read_csv(input_file)
@@ -41,91 +38,103 @@ def run_rf_classifier(input_file, work_folder):
     ## encode class label
     cmpt_class = 0
     old_label_to_encode = {}
-    for y in list(df['LABEL']):
-        if(y not in old_label_to_encode.keys()):
+    for y in list(df["LABEL"]):
+        if y not in old_label_to_encode.keys():
             old_label_to_encode[y] = cmpt_class
-            cmpt_class+=1
+            cmpt_class += 1
     df = df.replace(old_label_to_encode)
 
     ## extract features
-    features = [f for f in df.columns if f not in ['ID','LABEL']]
+    features = [f for f in df.columns if f not in ["ID", "LABEL"]]
 
-    ## prepare dataset
-    X = df[features].values
-    Y = df['LABEL'].values.ravel()
+    # run only if we have features
+    if len(features) > 0:
 
-    ## split into train & validation
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+        ## prepare dataset
+        X = df[features].values
+        Y = df["LABEL"].values.ravel()
 
-    ## init model
-    rf = RandomForestClassifier()
+        ## split into train & validation
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, Y, test_size=0.33, random_state=42
+        )
 
-    ## prepare for hyper param
-    #-> Number of trees in random forest
-    n_estimators = [int(x) for x in np.linspace(start = 5, stop = 2000, num = 5)]
-    #-> Number of features to consider at every split
-    max_features = ['auto', 'sqrt']
+        ## init model
+        rf = RandomForestClassifier()
 
-    #-> Maximum number of levels in tree
-    max_depth = [int(x) for x in np.linspace(3, 110, num = 5)]
-    max_depth.append(None)
+        ## prepare for hyper param
+        # -> Number of trees in random forest
+        n_estimators = [int(x) for x in np.linspace(start=5, stop=2000, num=5)]
+        # -> Number of features to consider at every split
+        max_features = ["auto", "sqrt"]
 
-    #-> Minimum number of samples required to split a node
-    min_samples_split = [2, 5, 10]
+        # -> Maximum number of levels in tree
+        max_depth = [int(x) for x in np.linspace(3, 110, num=5)]
+        max_depth.append(None)
 
-    #-> Minimum number of samples required at each leaf node
-    min_samples_leaf = [1, 2, 4]
+        # -> Minimum number of samples required to split a node
+        min_samples_split = [2, 5, 10]
 
-    #-> Method of selecting samples for training each tree
-    bootstrap = [True, False]
+        # -> Minimum number of samples required at each leaf node
+        min_samples_leaf = [1, 2, 4]
 
-    #-> Create the random grid
-    random_grid = {
-        'n_estimators': n_estimators,
-        'max_features': max_features,
-        'max_depth': max_depth,
-        'min_samples_split': min_samples_split,
-        'min_samples_leaf': min_samples_leaf,
-        'bootstrap': bootstrap
-    }
+        # -> Method of selecting samples for training each tree
+        bootstrap = [True, False]
 
-    clf = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+        # -> Create the random grid
+        random_grid = {
+            "n_estimators": n_estimators,
+            "max_features": max_features,
+            "max_depth": max_depth,
+            "min_samples_split": min_samples_split,
+            "min_samples_leaf": min_samples_leaf,
+            "bootstrap": bootstrap,
+        }
 
-    # Fit the random search model
-    clf.fit(X_train, y_train)
+        clf = RandomizedSearchCV(
+            estimator=rf,
+            param_distributions=random_grid,
+            n_iter=100,
+            cv=3,
+            verbose=2,
+            random_state=42,
+            n_jobs=-1,
+        )
 
-    ## pick the best
-    clf = clf.best_estimator_
+        # Fit the random search model
+        clf.fit(X_train, y_train)
 
-    ## save model
-    dump(clf, rf_save_name)
+        ## pick the best
+        clf = clf.best_estimator_
 
-    ## evaluate model
-    y_pred = clf.predict(X_test)
-    matrix = confusion_matrix(y_test, y_pred)
-    acc = accuracy_score(y_test, y_pred)
-    acc = acc*100.0
+        ## save model
+        dump(clf, rf_save_name)
 
-    ## display results
-    print("[+][CLF-RF] => ACC : "+str(acc)+" %")
+        ## evaluate model
+        y_pred = clf.predict(X_test)
+        matrix = confusion_matrix(y_test, y_pred)
+        acc = accuracy_score(y_test, y_pred)
+        acc = acc * 100.0
 
-    ## create confusion matrix figure
-    df_cm = pd.DataFrame(
-        matrix,
-        index = list(old_label_to_encode.keys()),
-        columns = list(old_label_to_encode.keys())
-    )
-    plt.figure(figsize = (10,7))
-    sn.heatmap(df_cm, annot=True, cmap='Blues')
-    plt.savefig(rf_confusion_save_file)
-    plt.close()
+        ## display results
+        print("[+][CLF-RF] => ACC : " + str(acc) + " %")
 
-    ## save acc in a log file
-    log_file = open(log_file_name, "w")
-    log_file.write("ACC\n")
-    log_file.write(str(acc)+"\n")
-    log_file.close()
+        ## create confusion matrix figure
+        df_cm = pd.DataFrame(
+            matrix,
+            index=list(old_label_to_encode.keys()),
+            columns=list(old_label_to_encode.keys()),
+        )
+        plt.figure(figsize=(10, 7))
+        sn.heatmap(df_cm, annot=True, cmap="Blues")
+        plt.savefig(rf_confusion_save_file)
+        plt.close()
+
+        ## save acc in a log file
+        log_file = open(log_file_name, "w")
+        log_file.write("ACC\n")
+        log_file.write(str(acc) + "\n")
+        log_file.close()
 
 
-
-#run_rf_classifier("../SSA/dataset/33_gene_sig_MCTD_classification.csv", "../misc/murloc_test")
+# run_rf_classifier("../SSA/dataset/33_gene_sig_MCTD_classification.csv", "../misc/murloc_test")
